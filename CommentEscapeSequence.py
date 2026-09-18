0448cb7ed9aaa1207e2c349e890c7511e2e785ca
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-18 22:50:10 +0530

    Comments and Escape Sequence in Python

---
+++ CommentEscapeSequence.py
@@ -0,0 +1,20 @@
+print("Hey I am learning python \n I have learned DSA in C++ previously")
+
+# '\n' is similar to endl --> called comments and \n is known is escape Sequence
+
+# Comments are used to make notes for future reference 
+
+'''
+This is a multi line comment 
+All the things inside triple quotes become multi line comments
+'''
+
+"""
+This is a multi line comment 
+All the things inside triple Double quotes become multi line comments
+"""
+
+print("Hey I am learning \"python\" \n I have learned DSA in C++ previously")
+
+print("Hey", "python", "Cpp", sep="->", end = "009\n")
+print("Utkarsh")
