f8470894f40ae34943d06f2835d0734dbccb0591
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-18 23:36:20 +0530

    Basic Calculator using python

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+print("Sum of both number is : ", a+b)
+print("Differnce of both number is : ", a-b)
+print("Multiplication of both number is : ", a*b)
+
+if(b==0):
+    print("Division by 0 is not possible")
+else:
+    print("division of both number is : ", a/b)
