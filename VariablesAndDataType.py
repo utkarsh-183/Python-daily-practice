a6289c506044427884c2496a7771907dbf202a9a
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-18 23:10:36 +0530

    Variables and data type in python

---
+++ VariablesAndDataType.py
@@ -0,0 +1,44 @@
+a = "Hii"
+print(a)
+
+b = 1
+print(b)
+
+c = 17
+print(b + c)
+
+d = True
+print(d)
+
+print("The type of a is : ", type(a))
+print("The type of b is : ", type(b))
+print("The type of c is : ", type(c))
+print("The type of a is : ", type(d))
+
+e = complex(2,3)
+print(e)
+
+list1 = [8, 2.3, [-4,5], ["apple", "banana"]]
+print(list1)
+
+# list is collection of element of different data types
+
+tuple1 = (("parrot", "sparrow"), ("lion", "tiger"))
+print(tuple1)
+
+#Tuple is collection of items that is ordered and immutable
+
+# List is mutable, but Tuple is immutable.
+mylist = [10, 20, 30]
+
+mylist[0] = 100
+
+print(mylist)
+
+
+# tuple is immutable
+# mytuple = (10, 20, 30)
+
+# mytuple[0] = 100
+
+# print(mytuple)
