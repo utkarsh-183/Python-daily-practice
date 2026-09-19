90a95adfd156c7b6ba71a405be92c147109d6ee9
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-20 00:12:34 +0530

    Break and Continue Statment

---
+++ breakAndContinue.py
@@ -0,0 +1,20 @@
+# for i in range(12):
+#     if(i==10):
+#         break
+#     print("5*", i+1, "=", 5*(i+1))
+
+# continue
+
+# for i in range(12):
+#     if(i==10):
+#         print("Skip the current iteration")
+#         continue
+#     print("5*", i+1, "=", 5*(i+1))
+
+# do-while 
+i = 0
+while True:
+    print(i)
+    i = i + 1
+    if(i%100==0):
+        break
