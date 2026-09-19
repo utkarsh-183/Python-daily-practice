0d8d5edb960e27c35fb0497de1801137b2051c38
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-20 00:11:21 +0530

    While Loop in python

---
+++ WhileLoop.py
@@ -0,0 +1,20 @@
+# i = 0
+# while(i<3):
+#     print(i)
+#     i = i + 1
+
+# print("Done with the loop")
+
+
+# i = int(input("Enter the number: "))
+# while(i<=18):
+#     i = int(input("Enter the number: "))
+#     print(i)
+
+count = 5
+
+while(count>0):
+    print(count)
+    count = count - 1
+else:
+    print("Condition becomes false and we are outside the loop")
