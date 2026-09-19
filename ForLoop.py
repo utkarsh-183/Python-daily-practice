6a0019e22ad531416c597ea3f09686fe0c1ca186
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-20 00:09:58 +0530

    For Loop in python

---
+++ ForLoop.py
@@ -0,0 +1,19 @@
+# name = "Utkarsh"
+
+# for i in name :
+#     print(i)
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print (i)
+
+for k in range(20):
+    print(k)
+
+for k in range(1, 9):
+    print(k)
+
+for k in range(1, 15, 2):  # staring no. -> 1, till 15-1, difference 2
+    print(k)
