0b767512246a2f640edc0a0e2a9e60bb9c45e346
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-19 09:56:52 +0530

    Strings in Python

---
+++ strings.py
@@ -0,0 +1,25 @@
+name = "Utkarsh"
+# print(name[0])
+
+# friend = "Sharib"
+# anotherFriend = "Shreyash"
+
+# print("hello", friend, "and", anotherFriend,",",name,"here" )
+
+# greetings = "hey 'Harry' \nI am 'Utkarsh' and i am learning 'python' from your 100 days of code."
+# print(greetings)
+
+
+# # we can print any paragraph using triple single quotes or triple double quotes
+
+para = """The Hanuman Chalisa is a Hindu devotional hymn in praise of Hanuman, and regularly recited by Hindus. 
+It was written by Tulsidas in the Awadhi language and is the best known text from the Ramcharitmanas.
+Hanuman ji is a Hindu deity and a devotee of the Hindu god, Rama."""
+
+# print(para)
+
+#let's use a for loop to print all characters of name
+
+for characters in name:
+    print(characters)
+
