52ffc00c18ca3250e0c6094687cdfd9372d6d2d1
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-21 09:51:06 +0530

    Tuple Methods

---
+++ tupleMethods.py
@@ -0,0 +1,12 @@
+# countries1 = ("India", "pakistan", "Australia", "canada")
+# countries2 = ("newZealand", "Denmark")
+
+# merge = countries1 + countries2
+# print(merge)
+
+tuple1 = (0,1,2,3,3,2,1,4,5,2)
+res = tuple1.count(3)
+print(res)
+
+res1 = tuple1.index(2)
+print(res1)
