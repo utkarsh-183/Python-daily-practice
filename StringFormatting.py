21a9dd5698a26239ed474b24b4bacc8a5f477d36
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-21 18:10:16 +0530

    Stirng Formatting

---
+++ StringFormatting.py
@@ -0,0 +1,22 @@
+letter = "Hey my name is {} and I am from {}"
+country = "India"
+name = "Utkarsh"
+
+
+# print(letter.format(name, country))
+
+# print(f"Hey my name is {name} and I am from {country}.")
+
+price = 49.09999
+txt = f"For only {price:.2f} dollars!"
+print(txt)
+
+name = "Utkarsh"
+age = 18
+
+print(f"My name is {name} and I am {age} years old.")
+
+a = 10
+b = 20
+
+print(f"Sum = {a + b}")
