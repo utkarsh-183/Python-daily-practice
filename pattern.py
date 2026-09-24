fb166474ba3bd9c8aef5ca90ad5672831d28d7a9
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-24 10:30:09 +0530

    Pattern printing in python

---
+++ pattern.py
@@ -0,0 +1,10 @@
+# for i in range(5):          # 5 rows
+#     for j in range(i + 1):  # stars in that row
+#         print("*", end="")
+#     print()
+
+
+for i in range (5):
+    for j in range (6):
+        print("*", end="")
+    print()
