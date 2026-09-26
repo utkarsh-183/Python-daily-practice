c69f16e2dd1fdfc066ee81f2c1f44ad4e4f63d8b
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-26 22:40:16 +0530

    DocStrings

---
+++ DocStrings.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    """This function adds two numbers."""
+    return a + b
+
+
+print(add.__doc__)
+print(add(4, 5))
+
+def square(n):
+    """This function returns the square of a number."""
+    return n * n
+
+
+print(square(5))
+print(square.__doc__)
